769ed1c084be790d9ca6b597e9b1381b418b7595
Anastasia Gromovaya <anastasialomejor@gmail.com>
2023-09-07 20:50:00 +0300

    updated and extended

--- lecture3/hoursminutes.py
+++ lecture3/hoursminutes.py
@@ -7,3 +7,30 @@ else:
     hours2 = hours1 // 60
     minutes = hours1 - hours2 * 60    
     print(f" {hours2} час(ов) и {minutes} минут(а/ы)")
+
+
+#much prettier
+
+num = int(input("Введите число в сек: "))
+
+
+hours1 = num // 60
+hours2 = hours1 // 60
+minutes = hours1 - hours2 * 60  
+
+if hours2 == 1:
+    timehours = "1 час"
+elif hours2 in [2, 3, 4]:
+    timehours = f"{hours2} часа"
+else:
+    timehours = f"{hours2} часов"
+
+if minutes == 1:
+    timeminutes = "1 минута"
+elif minutes in [2, 3, 4]:
+    timeminutes = f"{minutes} минуты"
+else:
+    timeminutes = f"{minutes} минут"
+
+print(f"{timehours} и {timeminutes}")
+
